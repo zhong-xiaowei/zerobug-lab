ba151c53d8f11d1beb801ff11654eb1f918236b8
zhongxiaowei <zhongxiaowei.nice@gamil.com>
2022-07-24 11:04:32 +0800

    ui

---
+++ zerobug-activiti-demo/src/main/java/cn/com/zerobug/demo/activiti/ActivitiDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.com.zerobug.demo.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerobugActivitiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerobugActivitiDemoApplication.class, args);
+    }
+
+}

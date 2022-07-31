b62946156114a7cf9181402c68f51a7e455a8378
zhongxiaowei <zhongxiaowei.nice@gamil.com>
2022-08-01 07:29:15 +0800

    shardingjdbc 使用demo

---
+++ zerobug-shardingjdbc-demo/src/main/java/cn/com/zerobug/ShardingJdbcApplication.java
@@ -0,0 +1,18 @@
+package cn.com.zerobug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhongxiaowei
+ * @contact zhongxiaowei.nice@gmail.com
+ * @date 2022/8/1
+ */
+@SpringBootApplication
+public class ShardingJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcApplication.class, args);
+    }
+
+}
